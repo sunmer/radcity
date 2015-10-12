977a79a1a948822b09c1663864c8f32157b80c91
nimaonsafari@gmail.com <nima.boustanian@designit.com>
2015-10-12 10:16:16 +0200

    consolidation

--- radcity.js
+++ radcity.js
@@ -3,51 +3,44 @@ var canvasWidth = 600;
 
 var gameSettings = { movementSpeed: 3, backgroundSpeed: 3, difficulty: 2, distanceBetweenEnemies: 200 };
 
-var assetPlayer = { 
-    res: 'assets/player_dogi.png', 
-    anim: 'playerAnim', 
-    animGameOver: 'playerAnimGameOver',
-    scale: { x: 0.2, y: 0.2 }, 
-    key: 'player' };
-
-var assetEnemy = {
+var assets = {
+    player: { res: 'assets/player_dogi.png', anim: 'playerAnim', animGameOver: 'playerAnimGameOver', key: 'player', scale: { x: 0.2, y: 0.2 } },
     enemies: [
-        { res: 'assets/enemy_jake.png', anim: 'enemyJakeAnim', key: 'enemyJake' },
-        { res: 'assets/enemy_nova.png', anim: 'enemyNovaAnim', key: 'enemyNova' }
+        { res: 'assets/enemy_jake.png', anim: 'enemyJakeAnim', key: 'enemyJake', scale: { x: 0.2, y: 0.2 } },
+        { res: 'assets/enemy_nova.png', anim: 'enemyNovaAnim', key: 'enemyNova', scale: { x: 0.2, y: 0.2 } }
     ],
-    scale: { x: 0.2, y: 0.2 }
-};
-
-var assetLevel = { res: 'assets/bg_level.png', anim: 'level1Anim', scale: { x: 1, y: 1 }, key: 'level1' };
+    level: { res: 'assets/bg_level.png', anim: 'level1Anim', scale: { x: 1, y: 1 }, key: 'level1' }
+}
 
 var states = {
     player: undefined,
     enemies: [],
+    projectiles: [],
     level: undefined,
     isGameOver: false,
     isPlayerMoving: false,
 
     preload: function() {
-        game.load.spritesheet(assetPlayer.key, assetPlayer.res, 140, 300, 4);
+        game.load.spritesheet(assets.player.key, assets.player.res, 140, 300, 4);
 
-        for(i = 0; i < assetEnemy.enemies.length; i++) {
-            game.load.spritesheet(assetEnemy.enemies[i].key, assetEnemy.enemies[i].res, 140, 300, 4);    
+        for(i = 0; i < assets.enemies.length; i++) {
+            game.load.spritesheet(assets.enemies[i].key, assets.enemies[i].res, 140, 300, 4);    
         }
         
-        game.load.image(assetLevel.key, assetLevel.res);
+        game.load.image(assets.level.key, assets.level.res);
 
-        game.load.spritesheet(assetPlayer.animGameOver, 'assets/player_dogi_gameover.png', 140, 300, 4);
+        game.load.spritesheet(assets.player.animGameOver, 'assets/player_dogi_gameover.png', 140, 300, 4);
     },
 
     create: function() {
-        this.level = game.add.tileSprite(0, 0, canvasWidth, game.cache.getImage(assetLevel.key).height, assetLevel.key);
+        this.level = game.add.tileSprite(0, 0, canvasWidth, game.cache.getImage(assets.level.key).height, assets.level.key);
         
-        this.player = game.add.sprite(game.world.centerX, game.world.centerY, assetPlayer.key);
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, assets.player.key);
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.body.collideWorldBounds = true;
         this.player.anchor.set(0.5);
-        this.player.scale.setTo(assetPlayer.scale.x, assetPlayer.scale.y);
-        this.player.animations.add(assetPlayer.anim)
+        this.player.scale.setTo(assets.player.scale.x, assets.player.scale.y);
+        this.player.animations.add(assets.player.anim)
 
         this.generateEnemy(gameSettings.difficulty);
     },
@@ -79,8 +72,12 @@ var states = {
                 this.isPlayerMoving = true;
             }
 
+            if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                console.log("shoot");
+            }
+
             if(this.isPlayerMoving) {
-                this.player.animations.play(assetPlayer.anim, 10, true);
+                this.player.animations.play(assets.player.anim, 10, true);
             } else {
                 this.player.animations.stop(null, true);
             }
@@ -118,16 +115,21 @@ var states = {
             enemy = game.add.sprite(
                 game.stage.getBounds().width + (Math.random() * gameSettings.distanceBetweenEnemies), 
                 Math.random() * (maxY - minY) + minY, 
-                assetEnemy.enemies[enemyType].key);
-            enemy.scale.setTo(assetEnemy.scale.x, assetEnemy.scale.y);
+                assets.enemies[enemyType].key);
 
-            enemyAnim = enemy.animations.add(assetEnemy.enemies[enemyType].anim);
-            enemy.animations.play(assetEnemy.enemies[enemyType].anim, 10, true);
+            enemy.scale.setTo(assets.enemies[enemyType].scale.x, assets.enemies[enemyType].scale.y);
+
+            enemyAnim = enemy.animations.add(assets.enemies[enemyType].anim);
+            enemy.animations.play(assets.enemies[enemyType].anim, 10, true);
 
             enemy.outOfBoundsKill = true;
 
             this.enemies.push(enemy);
         }
+    },
+
+    generateProjectile: function() {
+
     },
 
     destroyEnemy: function(enemy) {
@@ -142,7 +144,7 @@ var states = {
     },
 
     gameOver: function() {
-        this.player.loadTexture(assetPlayer.animGameOver, 0);
+        this.player.loadTexture(assets.player.animGameOver, 0);
         this.player.animations.add('gameOverAnim');
         this.player.animations.play('gameOverAnim', 10, true);
 
