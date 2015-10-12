1bc88b59cbe438eaeaa14ee794778d3dc2f22a47
nimaonsafari@gmail.com <nima.boustanian@designit.com>
2015-10-12 11:17:24 +0200

    shooting

--- radcity.js
+++ radcity.js
@@ -4,7 +4,26 @@ var canvasWidth = 600;
 var gameSettings = { movementSpeed: 3, backgroundSpeed: 3, difficulty: 2, distanceBetweenEnemies: 200 };
 
 var assets = {
-    player: { res: 'assets/player_dogi.png', anim: 'playerAnim', animGameOver: 'playerAnimGameOver', key: 'player', scale: { x: 0.2, y: 0.2 } },
+    player: {
+        projectile: {
+            res: 'assets/projectile.png',
+            key: 'projectile',
+            scale: { x: 0.3, y: 0.3 },
+            movementSpeed: 200
+        },
+        animations: {
+            movement: {
+                res: 'assets/player_dogi.png',
+                key: 'player',
+                scale: { x: 0.2, y: 0.2 }
+            },
+            gameOver: {
+                res: 'assets/player_dogi_gameover.png',
+                key: 'playerGameOver',
+                scale: { x: 0.2, y: 0.2 },
+            }
+        }
+    },
     enemies: [
         { res: 'assets/enemy_jake.png', anim: 'enemyJakeAnim', key: 'enemyJake', scale: { x: 0.2, y: 0.2 } },
         { res: 'assets/enemy_nova.png', anim: 'enemyNovaAnim', key: 'enemyNova', scale: { x: 0.2, y: 0.2 } }
@@ -21,26 +40,34 @@ var states = {
     isPlayerMoving: false,
 
     preload: function() {
-        game.load.spritesheet(assets.player.key, assets.player.res, 140, 300, 4);
+        //Non-animated assets
+        game.load.image(assets.level.key, assets.level.res);
+        game.load.image(assets.player.projectile.key, assets.player.projectile.res);
+
+        //Sprites and animations
+        game.load.spritesheet(assets.player.animations.movement.key, assets.player.animations.movement.res, 140, 300, 4);
+        game.load.spritesheet(assets.player.animations.gameOver.key, assets.player.animations.gameOver.res, 140, 300, 4);
 
         for(i = 0; i < assets.enemies.length; i++) {
             game.load.spritesheet(assets.enemies[i].key, assets.enemies[i].res, 140, 300, 4);    
         }
-        
-        game.load.image(assets.level.key, assets.level.res);
-
-        game.load.spritesheet(assets.player.animGameOver, 'assets/player_dogi_gameover.png', 140, 300, 4);
     },
 
     create: function() {
+        //Assets
         this.level = game.add.tileSprite(0, 0, canvasWidth, game.cache.getImage(assets.level.key).height, assets.level.key);
         
-        this.player = game.add.sprite(game.world.centerX, game.world.centerY, assets.player.key);
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, assets.player.animations.movement.key);
         game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
         this.player.body.collideWorldBounds = true;
         this.player.anchor.set(0.5);
-        this.player.scale.setTo(assets.player.scale.x, assets.player.scale.y);
-        this.player.animations.add(assets.player.anim)
+        this.player.scale.setTo(assets.player.animations.movement.scale.x, assets.player.animations.movement.scale.y);
+        this.player.animations.add(assets.player.animations.movement.key);
+
+        //Listeners
+        projectile = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        projectile.onDown.add(this.generateProjectile, this);
 
         this.generateEnemy(gameSettings.difficulty);
     },
@@ -72,12 +99,8 @@ var states = {
                 this.isPlayerMoving = true;
             }
 
-            if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-                console.log("shoot");
-            }
-
             if(this.isPlayerMoving) {
-                this.player.animations.play(assets.player.anim, 10, true);
+                this.player.animations.play(assets.player.animations.movement.key, 10, true);
             } else {
                 this.player.animations.stop(null, true);
             }
@@ -129,11 +152,20 @@ var states = {
     },
 
     generateProjectile: function() {
+        if(this.projectiles.length > 2) {
+            this.projectiles[0].destroy();
+            this.projectiles.splice(0, 1);
 
-    },
+            return;
+        }
 
-    destroyEnemy: function(enemy) {
+        var projectile = game.add.sprite(this.player.x, this.player.y, assets.player.projectile.key);
+        game.physics.enable(projectile, Phaser.Physics.ARCADE);
+        projectile.anchor.set(0.5);
+        projectile.scale.setTo(assets.player.projectile.scale.x, assets.player.projectile.scale.y);
+        projectile.body.velocity.x += assets.player.projectile.movementSpeed;
 
+        this.projectiles.push(projectile);
     },
 
     checkOverlap: function(spriteA, spriteB) {
@@ -144,9 +176,9 @@ var states = {
     },
 
     gameOver: function() {
-        this.player.loadTexture(assets.player.animGameOver, 0);
-        this.player.animations.add('gameOverAnim');
-        this.player.animations.play('gameOverAnim', 10, true);
+        this.player.loadTexture(assets.player.animations.gameOver.key, 0);
+        this.player.animations.add(assets.player.animations.gameOver.key);
+        this.player.animations.play(assets.player.animations.gameOver.key, 10, true);
 
         for(var i = 0; i < this.enemies.length; i++) {
             this.enemies[i].animations.stop(null, true);
